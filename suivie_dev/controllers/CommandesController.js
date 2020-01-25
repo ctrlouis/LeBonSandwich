9c2888315bc6c077f0475b75a5e7b7b83bee4edc
ctrlouis <louis.bty57@gmail.com>
2020-01-25 17:12:12 +0100

    add request commandes by id to allow metadata links usage

--- suivie_dev/controllers/CommandesController.js
+++ suivie_dev/controllers/CommandesController.js
@@ -21,52 +21,81 @@ class CommandesController {
 
         countQuery.then((rowNumber) => {
 
-                const pagination = CommandesController.pagination(req, rowNumber[0].count);
+            const pagination = CommandesController.pagination(req, rowNumber[0].count);
 
-                let query = db.select('*').from(table)
-                    .limit(pagination.size)
-                    .offset(pagination.queryOffset);
+            let query = db.select('*').from(table)
+                .limit(pagination.size)
+                .offset(pagination.queryOffset);
 
-                query = CommandesController.filter(req, query);
+            query = CommandesController.filter(req, query);
 
-                query.then((result) => {
+            query.then((result) => {
 
-                        // init collection with meta data
-                        let collection = {
-                            type: "collection",
-                            count: rowNumber[0].count,
-                            size: pagination.size,
-                            commandes: []
-                        }
+                    // init collection with meta data
+                    let collection = {
+                        type: "collection",
+                        count: rowNumber[0].count,
+                        size: pagination.size,
+                        commandes: []
+                    }
 
-                        // add pagination metadata
-                        collection.links = Pagination.getLinks(pagination, "/commands");
+                    // add pagination metadata
+                    collection.links = Pagination.getLinks(pagination, "/commands");
 
-                        // add data into collection
-                        result.forEach((commande) => {
-                            collection.commandes.push({
-                                command: {
-                                    id: commande.id,
-                                    nom: commande.nom,
-                                    create_at: commande.create_at,
-                                    livraison: commande.livraison.toLocaleDateString('fr-FR', ) + " " + commande.livraison.toLocaleTimeString('fr-FR', ),
-                                    status: commande.status
-                                },
-                                links: {
-                                    self: {
-                                        href: "/commands/" + commande.id
-                                    }
+                    // add data into collection
+                    result.forEach((commande) => {
+                        collection.commandes.push({
+                            command: {
+                                id: commande.id,
+                                nom: commande.nom,
+                                create_at: commande.create_at,
+                                livraison: commande.livraison.toLocaleDateString('fr-FR', ) + " " + commande.livraison.toLocaleTimeString('fr-FR', ),
+                                status: commande.status
+                            },
+                            links: {
+                                self: {
                                     href: "/commandes/" + commande.id
                                 }
-                            });
+                            }
                         });
+                    });
 
-                        // return collection
-                        res.json(collection);
-                    })
-                    .catch((error) => res.status(500).json(Error.create(500, error)));
-            });
+                    // return collection
+                    res.json(collection);
+                })
+                .catch((error) => res.status(500).json(Error.create(500, error)));
+        });
     }
+
+    /*
+     * Get by id
+     */
+    static id(req, res) {
+        db.select()
+            .table(table)
+            .where('id', req.params.id)
+            .then((result) => {
+                // if no ressource catch
+                if (result <= 0) res.status(404).json(Error.create(404, "Ressource not available: " + req.originalUrl));
+                let collec = {
+                    type: "ressource",
+                    commande: []
+                };
+                result.forEach(cmd => {
+                    collec.commande.push({
+                        id: cmd.id,
+                        mail_client: cmd.mail,
+                        nom_client: cmd.nom,
+                        date_commande: cmd.created_at,
+                        date_livraison: cmd.livraison,
+                        montant: cmd.montant
+                    })
+                });
+                res.json(collec);
+            })
+            .catch((error) => console.error(error));
+    }
+
 
     static filter(req, query) {
         // filter by status value
--- suivie_dev/routes/commandesRouter.js
+++ suivie_dev/routes/commandesRouter.js
@@ -10,6 +10,9 @@ const router = express.Router();
 /* GET commandes listing. */
 router.get('/',     CommandesController.all);
 
+/* GET commande by given id. */
+router.get('/:id',  CommandesController.id);
+
 /* Error handler */
 router.all('/*',    (req, res) => res.status(405).json(Error.create(405, "Method not allowed")));
 
