b328be3d1d02d49adbd6083a08bfe13062259ffc
Victor Bour <victor.bour.bowden@gmail.com>
2020-01-29 15:29:04 +0100

    Return a categorie formatted like a hateoas link

--- catalogue_dev/controllers/CategorieController.js
+++ catalogue_dev/controllers/CategorieController.js
@@ -22,10 +22,21 @@ class CategorieController {
      * Get by id
      */
     static id(req, res) {
+        let objet = {
+            type: "ressource",
+            date: new Date().toLocaleDateString('fr-FR', {day: '2-digit', month: '2-digit', year:'numeric'}),
+            categorie: {},
+            links: {}
+        };
         ConnectionFactory.connect();
-        CategorieModel.find({id: Number(req.params.id)}, function(err, categories){
+        CategorieModel.findOne({id: Number(req.params.id)}, function(err, categorie){
             if (err) return handleError(err);
-            res.json(categories);
+            objet.categorie.id = categorie.id;
+            objet.categorie.nom = categorie.nom;
+            objet.categorie.description = categorie.description;
+            objet.links.sandwichs = {href: "/categories/" + categorie.id + "/sandwichs/"};
+            objet.links.self = {href: "/categories/" + categorie.id + "/"};
+            res.json(objet);
         });
     }
 }
--- catalogue_dev/schema/CategorieSchema.js
+++ catalogue_dev/schema/CategorieSchema.js
@@ -9,8 +9,11 @@ const ObjectId = Schema.ObjectId;
 const collection = 'categories';
 
 export const CategorieSchema = new Schema({
-        nom: String,
-        description: String
+        id: {type:Number},
+        nom: {type: String, required: true},
+        description: {type:String, default: ''}
+}, {
+    versionKey: false
 });
 
 export const CategorieModel = mongoose.model(collection, CategorieSchema);
