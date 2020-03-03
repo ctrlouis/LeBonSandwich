8047c9e8f753cdded6d9e38e7d1d480715852c52
Victor Bour <victor.bour.bowden@gmail.com>
2020-03-03 16:37:42 +0100

    Add verification of name, mail and date format

--- commande_dev/controllers/CommandesController.js
+++ commande_dev/controllers/CommandesController.js
@@ -90,83 +90,90 @@ class CommandesController {
      */
     static create(req, res) {
         const id = uuid(); // generate id
-        bcrypt.hash(id, saltRounds, function (err, token) {
+        let validatedData = CommandesController.validatedData(req.body)
+        if(!validatedData.valid){
+            res.status(500).json(Error.create(500, validatedData.error));
+        }
+        else
+        {
+            bcrypt.hash(id, saltRounds, function (err, token) {
 
-            const items = req.body.items;
-            const requests = [];
-            items.forEach(item => requests.push(CommandesController.getSandwichRequest(item.uri)));
+                const items = req.body.items;
+                const requests = [];
+                items.forEach(item => requests.push(CommandesController.getSandwichRequest(item.uri)));
 
-            // get all used sandwichs
-            axios.all(requests)
-                .then(axios.spread(function (...sandwichs) {
+                // get all used sandwichs
+                axios.all(requests)
+                    .then(axios.spread(function (...sandwichs) {
 
-                    // new commande
-                    const insertData = {
-                        nom: req.body.nom,
-                        mail: req.body.mail,
-                        livraison: Tools.createDate(req.body.livraison.date, req.body.livraison.heure),
-                        id: uuid(),
-                        token: token,
-                        created_at: new Date()
-                    };
+                        // new commande
+                        const insertData = {
+                            nom: req.body.nom,
+                            mail: req.body.mail,
+                            livraison: new Date(req.body.livraison.date + ' ' + req.body.livraison.heure),
+                            id: uuid(),
+                            token: token,
+                            created_at: new Date()
+                        };
 
-                    let insertItems = [];
-                    sandwichs.forEach((sandwich) => {                        
-                        const quantity = req.body.items.find(e => e.uri == "/sandwichs/" + sandwich.data.ref); // get corresponding quantity
-                        insertItems.push({
-                            uri: '/sandwichs/' + sandwich.data.ref,
-                            libelle: sandwich.data.nom,
-                            tarif: sandwich.data.prix.$numberDecimal,
-                            quantite: quantity.q,
-                            command_id: insertData.id
+                        let insertItems = [];
+                        sandwichs.forEach((sandwich) => {
+                            const quantity = req.body.items.find(e => e.uri == "/sandwichs/" + sandwich.data.ref); // get corresponding quantity
+                            insertItems.push({
+                                uri: '/sandwichs/' + sandwich.data.ref,
+                                libelle: sandwich.data.nom,
+                                tarif: sandwich.data.prix.$numberDecimal,
+                                quantite: quantity.q,
+                                command_id: insertData.id
+                            });
                         });
-                    });         
-                    
-                    let amount = 0;
-                    insertItems.forEach(item => amount += item.quantite * item.tarif);
-                    insertData.montant = amount;
 
-                    // insert items
-                    db.insert(insertItems).table('item')
-                        .then((res) => {})
-                        .catch((err) => console.log(err));
+                        let amount = 0;
+                        insertItems.forEach(item => amount += item.quantite * item.tarif);
+                        insertData.montant = amount;
 
-                    db.insert(insertData).into(table)
-                        .then((result) => {
-                            
-                            db.select()
-                                .table(table)
-                                .where('id', insertData.id)
-                                .first()
-                                .then((result) => {
-                                    if (!result) res.status(404).json(Error.create(404, "Ressource not available: " + req.originalUrl));
+                        // insert items
+                        db.insert(insertItems).table('item')
+                            .then((res) => {})
+                            .catch((err) => console.log(err));
 
-                                    const newCommandeFormated = {
-                                        commande: {
-                                            nom: insertData.nom,
-                                            mail: insertData.mail,
-                                            livraison: {
-                                                date: Tools.formatDate(insertData.livraison),
-                                                heure: Tools.formatHour(insertData.livraison)
-                                            },
-                                            id: insertData.id,
-                                            token: insertData.token,
-                                            montant: insertData.montant,
-                                            items: items
-                                        }
-                                    };
+                        db.insert(insertData).into(table)
+                            .then((result) => {
 
-                                    res.status(201).location('/commandes/' + insertData.id).json(newCommandeFormated);
-                                })
-                                .catch((error) => res.status(500).json(Error.create(500, error)));
-                                // end of selection to return value
-                        })
-                        .catch((error) => res.status(500).json(Error.create(500, error)));
-                        // end of insertion
-                }))
-                .catch((err) => res.status(400).json(err));
-                // end of multiple get sandwich
-        });
+                                db.select()
+                                    .table(table)
+                                    .where('id', insertData.id)
+                                    .first()
+                                    .then((result) => {
+                                        if (!result) res.status(404).json(Error.create(404, "Ressource not available: " + req.originalUrl));
+
+                                        const newCommandeFormated = {
+                                            commande: {
+                                                nom: insertData.nom,
+                                                mail: insertData.mail,
+                                                livraison: {
+                                                    date: Tools.formatDate(insertData.livraison),
+                                                    heure: Tools.formatHour(insertData.livraison)
+                                                },
+                                                id: insertData.id,
+                                                token: insertData.token,
+                                                montant: insertData.montant,
+                                                items: items
+                                            }
+                                        };
+
+                                        res.status(201).location('/commandes/' + insertData.id).json(newCommandeFormated);
+                                    })
+                                    .catch((error) => res.status(500).json(Error.create(500, error)));
+                                    // end of selection to return value
+                            })
+                            .catch((error) => res.status(500).json(Error.create(500, error)));
+                            // end of insertion
+                    }))
+                    .catch((err) => res.status(400).json(err));
+                    // end of multiple get sandwich
+            });
+        }
     }
 
     /*
@@ -223,6 +230,44 @@ class CommandesController {
     static getSandwichRequest(uri) {
         const url = "http://api.catalogue:8080" + uri;
         return axios.get(url);
+    }
+
+    // confirm dataset format respecting table's format
+    static validatedData(dataset){
+        let res = {valid: true, error: 'Bad format'};
+        //Mail check
+        if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(dataset.mail)){
+            res.valid = false;
+            res.error = 'Bad mail format';
+        }
+        //Name check (ex: Victor Bour = 1, VictR Bour = 0)
+        if(!/^[A-Z]?[a-z]{2,}\s[a-zA-Z]{2,}$/.test(dataset.nom)){
+            res.valid = false;
+            res.error = 'Bad firstname and lastname format'
+        }
+        //check date format
+        if(dataset.livraison){
+            let date = new Date(dataset.livraison.date + ' ' + dataset.livraison.heure);
+            date.setHours(date.getHours() - 1); //Evite l'ajout inutile d'une heure (GMT), qui brise la comparaison
+            if(Date.parse(date) < Date.now() || date == 'Invalid Date'){
+                res.valid = false;
+                res.error = 'Date de livraison invalide';
+            }
+        }
+        else{
+            res.valid = false;
+            res.error = 'Date de livraison invalide';
+        }
+
+        if(dataset.items){
+
+        }
+        else{
+            res.valid = false;
+            res.error = 'Liste d\'items invalide';
+        }
+
+        return res;
     }
 
 }
